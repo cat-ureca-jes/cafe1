623adc226acc795b106d5275d038b0629b5e82e3
JaenSTD02 <JaenSTD02@jaenst02>
2025-03-05 11:48:56 +0900

    프로젝트 구성

--- src/main/java/com/shop/cafe/dao/ProductDao.java
+++ src/main/java/com/shop/cafe/dao/ProductDao.java
@@ -3,6 +3,7 @@ package com.shop.cafe.dao;
 import java.sql.*;
 import java.util.*;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.shop.cafe.dto.Product;
@@ -10,16 +11,28 @@ import com.shop.cafe.dto.Product;
 @Component
 public class ProductDao {
 	
+	@Value("${spring.datasource.driver-class-name}")
+	private String DB_DRIVER;
+	
+	@Value("${spring.datasource.url}")
+	private String DB_URL;
+	
+	@Value("${spring.datasource.username}")
+	private String DB_USER;
+	
+	@Value("${spring.datasource.password}")
+	private String DB_PW;
+	
+	
+	
 	public List<Product> getAllProducts() throws Exception{
-		Class.forName("com.mysql.cj.jdbc.Driver");
-		String url="jdbc:mysql://localhost:3306/ureca?serverTimezone=UTC";
-		String user="ureca";
-		String pw="ureca";
+		
+		Class.forName(DB_DRIVER);		
 		
 		String sql="select * from product";
 		
 		try(
-		Connection con=DriverManager.getConnection(url,user,pw);
+		Connection con=DriverManager.getConnection(DB_URL,DB_USER,DB_PW);
 		PreparedStatement stmt=con.prepareStatement(sql);
 		ResultSet rs=stmt.executeQuery();
 				){
